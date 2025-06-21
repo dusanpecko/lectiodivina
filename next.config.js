6277daf9f8eaf0e18b6ad38dd5f1bf837e5bdc3f
Dušan Pecko <dusan@pecko.me>
2025-06-21 23:22:02 +0200

    v_1.0

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone', // potrebné pre produkčné spustenie bez servera
+}
+
+module.exports = nextConfig
