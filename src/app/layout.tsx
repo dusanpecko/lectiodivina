c1c298de07bbd5ee96fbd4d04f408094dfa71dc1
Dušan Pecko <dusan@pecko.me>
2025-06-28 09:09:41 +0200

    unami integracia

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import SupabaseProvider from './components/SupabaseProvider'
 import { LanguageProvider } from './components/LanguageProvider'
 import { CookieConsentProvider } from './components/CookieConsentContext'
 import Footer from './components/Footer'
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata = {
@@ -27,6 +28,13 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="format-detection" content="telephone=no" />
+        
+        {/* Umami Analytics */}
+        <Script
+          src="https://analytics.lectiodivina.org/script.js"
+          data-website-id="c7311d25-b9b7-4c15-9d15-333962066cdb"
+          strategy="afterInteractive"
+        />
       </head>
       <body className="min-h-screen overflow-x-hidden bg-white text-black" suppressHydrationWarning>
         <CookieConsentProvider>
